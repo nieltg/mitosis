7dd862ca9965b17385e39a643bf2552ebab32fc2
Jonathan Christopher <jonathan.christopher@outlook.com>
2017-04-22 17:02:31 +0700

    Added file header

---
+++ .idea/fileTemplates/includes/File Header.java
@@ -0,0 +1,14 @@
+/**
+ * Mitosis - Tugas Besar IF2210 Pemrograman Berorientasi Objek
+ * Kelompok 1 - SterileCode
+ * - 13515001 [K-01] Jonathan Christopher
+ * - 13515002 [K-02] Wenny Yustalim
+ * - 13515071 [K-02] Daniel Pintara
+ * - 13515093 [K-03] Reinaldo Ignatius
+ * ***
+ * Nama file         : ${NAME}.java
+ * Tanggal dibuat    : ${DATE}
+ * Tanggal perubahan : ${DATE}
+ * Deskripsi         : -
+ */
+ 
