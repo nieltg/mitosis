e07ac6c38ba4d0822a8d39a5bb97661f8e8cbfd5
Jonathan Christopher <jonathan.christopher@outlook.com>
2017-04-22 19:12:11 +0700

    Changed file header to English

--- .idea/fileTemplates/includes/File Header.java
+++ .idea/fileTemplates/includes/File Header.java
@@ -1,14 +1,15 @@
-/**
- * Mitosis - Tugas Besar IF2210 Pemrograman Berorientasi Objek
- * Kelompok 1 - SterileCode
+
+
+/*
+ * Mitosis - IF2210 Object-oriented Programming
+ * Group 1 - SterileCode
  * - 13515001 [K-01] Jonathan Christopher
  * - 13515002 [K-02] Wenny Yustalim
  * - 13515071 [K-02] Daniel Pintara
  * - 13515093 [K-03] Reinaldo Ignatius
  * ***
- * Nama file         : ${NAME}.java
- * Tanggal dibuat    : ${DATE}
- * Tanggal perubahan : ${DATE}
- * Deskripsi         : -
+ * File name         : ${NAME}.java
+ * Created at        : ${DATE}
+ * Last modified at  : ${DATE}
  */
  
