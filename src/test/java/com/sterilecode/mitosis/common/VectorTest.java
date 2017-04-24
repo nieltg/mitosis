34f9bcc05eb2603a7a9bf94b35881024bf31c853
Wenny Yustalim <wennyyustalim@gmail.com>
2017-04-24 17:36:50 +0700

    add unit tests, straightbehavior not checked

--- src/test/java/com/sterilecode/mitosis/common/VectorTest.java
+++ src/test/java/com/sterilecode/mitosis/common/VectorTest.java
@@ -2,63 +2,76 @@ package com.sterilecode.mitosis.common;
 
 import org.junit.Test;
 import java.lang.Math;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by wennyyustalim on 4/24/17.
+/*
+ * Mitosis - IF2210 Object-oriented Programming
+ * Group 1 - SterileCode
+ * - 13515001 [K-01] Jonathan Christopher
+ * - 13515002 [K-02] Wenny Yustalim
+ * - 13515071 [K-02] Daniel Pintara
+ * - 13515093 [K-03] Reinaldo Ignatius
+ * ***
+ * File name         : ${NAME}.java
+ * Created at        : 4/24/17
+ * Last modified at  : 4/24/17
  */
 public class VectorTest {
-    Vector v1 = new Vector();
-    Vector v2 = new Vector(3,4);
-    @Test
-    public void getX() throws Exception {
-        assertEquals(0.0d, v1.getX(), 0);
-    }
 
-    @Test
-    public void getY() throws Exception {
-        assertEquals(0.0d, v1.getY(), 0);
-    }
+  Vector v1 = new Vector();
+  Vector v2 = new Vector(3, 4);
+  private static final double EPS = 1e-9;
 
-    @Test
-    public void setX() throws Exception {
-        v1.setX(3.0d);
-        assertEquals(3.0d, v1.getX(), 0);
-    }
+  @Test
+  public void getX() throws Exception {
+    assertEquals(0.0d, v1.getX(), EPS);
+  }
 
-    @Test
-    public void setY() throws Exception {
-        v1.setY(4.0d);
-        assertEquals(4.0d, v1.getY(), 0);
-    }
+  @Test
+  public void getY() throws Exception {
+    assertEquals(0.0d, v1.getY(), EPS);
+  }
 
-    @Test
-    public void equals() throws Exception {
-        Vector v3 = new Vector();
-        assertTrue(v1.equals(v3));
-    }
+  @Test
+  public void setX() throws Exception {
+    v1.setX(3.0d);
+    assertEquals(3.0d, v1.getX(), EPS);
+  }
 
-    @Test
-    public void add() throws Exception {
-        Vector v3 = new Vector();
-        v3 = v1.add(v2);
-        assertEquals(3, v3.getX(), 0);
-    }
+  @Test
+  public void setY() throws Exception {
+    v1.setY(4.0d);
+    assertEquals(4.0d, v1.getY(), EPS);
+  }
 
-    @Test
-    public void multiply() throws Exception {
-        Vector v3 = new Vector();
-        v3 = v2.multiply(3);
-        assertEquals(9, v3.getX(), 0);
-        assertEquals(12, v3.getY(), 0);
-    }
+  @Test
+  public void equals() throws Exception {
+    Vector v3 = new Vector();
+    assertTrue(v1.equals(v3));
+  }
 
-    @Test
-    public void rotate() throws Exception {
-        Vector v3 = new Vector();
-        v3 = v2.rotate(0.5*Math.PI);
-        assertEquals(-4, v3.getX(), 0);
-        assertEquals(3, v3.getY(), 0.0001);
-    }
+  @Test
+  public void add() throws Exception {
+    Vector v3 = new Vector();
+    v3 = v1.add(v2);
+    assertEquals(3, v3.getX(), EPS);
+  }
+
+  @Test
+  public void multiply() throws Exception {
+    Vector v3 = new Vector();
+    v3 = v2.multiply(3);
+    assertEquals(9, v3.getX(), EPS);
+    assertEquals(12, v3.getY(), EPS);
+  }
+
+  @Test
+  public void rotate() throws Exception {
+    Vector v3 = new Vector();
+    v3 = v2.rotate(0.5 * Math.PI);
+    assertEquals(-4, v3.getX(), EPS);
+    assertEquals(3, v3.getY(), EPS);
+  }
 
 }
---
+++ src/test/java/com/sterilecode/mitosis/model/behavior/straightbehavior/StraightBehaviorTest.java
@@ -0,0 +1,42 @@
+package com.sterilecode.mitosis.model.behavior.straightbehavior;
+
+import com.sterilecode.mitosis.common.Vector;
+import com.sterilecode.mitosis.model.gameobject.GameObject;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+
+/*
+ * Mitosis - IF2210 Object-oriented Programming
+ * Group 1 - SterileCode
+ * - 13515001 [K-01] Jonathan Christopher
+ * - 13515002 [K-02] Wenny Yustalim
+ * - 13515071 [K-02] Daniel Pintara
+ * - 13515093 [K-03] Reinaldo Ignatius
+ * ***
+ * File name         : StraightBehaviorTest.java
+ * Created at        : 4/24/17
+ * Last modified at  : 4/24/17
+ */
+
+public class StraightBehaviorTest {
+  @Mock
+  GameObject gobj;
+  @Rule
+      public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+  @Test
+  public void move() throws Exception {
+    StraightBehavior sb = new StraightBehavior(gobj);
+    Vector pos = new Vector(1,2);
+    gobj.setPosition(pos);
+    sb.move(1000000);
+    assertEquals(0.001, gobj.getPosition().getX(), 0.001);
+  }
+}
