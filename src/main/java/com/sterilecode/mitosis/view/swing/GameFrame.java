95fdd32d276671ac0dea94e4a53c501e2debdec1
Jonathan Christopher <jonathan.christopher@outlook.com>
2017-04-24 16:30:05 +0700

    Working input

--- src/main/java/com/sterilecode/mitosis/view/swing/GameFrame.java
+++ src/main/java/com/sterilecode/mitosis/view/swing/GameFrame.java
@@ -13,19 +13,29 @@ package com.sterilecode.mitosis.view.swing;
  * Last modified at  : 4/24/17
  */
 
-import com.sterilecode.mitosis.controller.GameDevice;
+import com.sterilecode.mitosis.view.GameDevice;
+import com.sterilecode.mitosis.view.InputState;
 import java.awt.Dimension;
 import java.awt.Insets;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
 import javax.swing.JFrame;
 
-public class GameFrame extends JFrame implements GameDevice {
+public class GameFrame extends JFrame implements GameDevice, KeyListener {
 
   private static final int BUFFER_WIDTH = 720;
   private static final int BUFFER_HEIGHT = 480;
   private static final int BUFFER_COUNT = 2;
   private Insets insets;
+  private InputState inputState;
+  private Map<Integer, Consumer<Boolean>> keyMap;
 
   public GameFrame() {
+
+    // Set up window and drawing surface
     setTitle("Mitosis");
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setIgnoreRepaint(true);
@@ -35,6 +45,22 @@ public class GameFrame extends JFrame implements GameDevice {
     insets = this.getInsets();
     setVisible(true);
     createBufferStrategy(BUFFER_COUNT);
+
+    // Prepare to receive input
+    inputState = new InputState();
+
+    // Set up key map
+    keyMap = new HashMap<>();
+    keyMap.put(KeyEvent.VK_ESCAPE, inputState::setMenuKeyPressed);
+    keyMap.put(KeyEvent.VK_LEFT, inputState::setPlayer1RotateLeftKeyPressed);
+    keyMap.put(KeyEvent.VK_RIGHT, inputState::setPlayer1RotateRightKeyPressed);
+    keyMap.put(KeyEvent.VK_DOWN, inputState::setPlayer1ShootKeyPressed);
+    keyMap.put(KeyEvent.VK_A, inputState::setPlayer2RotateLeftKeyPressed);
+    keyMap.put(KeyEvent.VK_D, inputState::setPlayer2RotateRightKeyPressed);
+    keyMap.put(KeyEvent.VK_S, inputState::setPlayer2ShootKeyPressed);
+
+    // Begin listening for key input events
+    addKeyListener(this);
   }
 
   @Override
@@ -55,5 +81,38 @@ public class GameFrame extends JFrame implements GameDevice {
   @Override
   public int getBufferY() {
     return insets.top;
+  }
+
+  @Override
+  public InputState getInputState() {
+    return inputState;
+  }
+
+  @Override
+  public void keyTyped(KeyEvent e) {
+  }
+
+  /**
+   * Handles key press event; set the corresponding state in input state to true.
+   * e Key event details.
+   */
+  @Override
+  public void keyPressed(KeyEvent e) {
+    Consumer<Boolean> keySetMethod = keyMap.get(e.getKeyCode());
+    if (keySetMethod != null) {
+      keySetMethod.accept(true);
+    }
+  }
+
+  /**
+   * Handles key release event; set the corresponding state in input state to false.
+   * e Key event details.
+   */
+  @Override
+  public void keyReleased(KeyEvent e) {
+    Consumer<Boolean> keySetMethod = keyMap.get(e.getKeyCode());
+    if (keySetMethod != null) {
+      keySetMethod.accept(false);
+    }
   }
 }
