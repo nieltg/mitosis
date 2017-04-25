4d155932d5b8278ecf13972354f67e13c5f251e4
Daniel Gunawan <nieltansah@gmail.com>
2017-04-26 00:20:54 +0700

    plugin: Add ability to notify RegistryListener
    Note: Use safer impl. of objectDisposed() event listener

--- src/main/java/com/sterilecode/mitosis/plugin/PluginRegistry.java
+++ src/main/java/com/sterilecode/mitosis/plugin/PluginRegistry.java
@@ -1,5 +1,6 @@
 package com.sterilecode.mitosis.plugin;
 
+import com.sterilecode.mitosis.plugin.client.RegistryListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,12 +12,26 @@ public class PluginRegistry {
   private static final PluginRegistry pluginRegistryInstance = new PluginRegistry();
 
   private final Map<String, List<ObjectSupplier>> knownSupplies = new HashMap<>();
+  private final Map<String, List<RegistryListener>> registryListeners = new HashMap<>();
 
   private final ObjectDisposalListener supplyDisposalListener = new ObjectDisposalListener() {
     @Override
     public void objectDisposed(ObjectSupplier source) {
-      List<ObjectSupplier> supplyList = knownSupplies.get(source.getObjectId());
-      supplyList.remove(source);
+      String objectId = source.getObjectId();
+      List<ObjectSupplier> supplyList = knownSupplies.get(objectId);
+
+      if (supplyList != null) {
+
+        if (supplyList.remove(source)) {
+          List<RegistryListener> listenerList = registryListeners.get(objectId);
+
+          listenerList.forEach(listener ->
+              listener.registryObjectRemoved(objectId, source.getObject()));
+        }
+
+        if (supplyList.isEmpty())
+          knownSupplies.remove(objectId);
+      }
 
       source.removeDisposalListener(this);
     }
@@ -40,15 +55,43 @@ public class PluginRegistry {
     }
 
     supplyList.add(supply);
-
     supply.addDisposalListener(supplyDisposalListener);
+
+    // Notify listeners.
+
+    List<RegistryListener> listenerList = registryListeners.get(objectId);
+
+    if (listenerList != null)
+      listenerList.forEach(listener -> listener.registryObjectAdded(objectId, supply.getObject()));
   }
 
   public List<Object> getObjects(String objectId) {
     List<ObjectSupplier> supplies = knownSupplies.get(objectId);
 
     return supplies.stream()
-        .map(supply -> supply.getObject())
+        .map(ObjectSupplier::getObject)
         .collect(Collectors.toList());
+  }
+
+  public void addRegistryListener(String serviceId, RegistryListener listener) {
+    List<RegistryListener> listenerList = registryListeners.get(serviceId);
+
+    if (listenerList == null) {
+      listenerList = new ArrayList<>();
+      registryListeners.put(serviceId, listenerList);
+    }
+
+    listenerList.add(listener);
+  }
+
+  public void removeRegistryListener(String serviceId, RegistryListener listener) {
+    List<RegistryListener> listenerList = registryListeners.get(serviceId);
+
+    if (listenerList != null) {
+      listenerList.remove(listener);
+
+      if (listenerList.isEmpty())
+        registryListeners.remove(serviceId);
+    }
   }
 }
